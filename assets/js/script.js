37e309d774f62536d8570f570af10009bfc08abf
Lash Blast <evelyn.aguirre.9895@gmail.com>
2022-03-14 16:36:08 -0500

    verify api response

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,27 @@
 // 7 day forecast w alerts
-// https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
+// https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude=minutely,hourly,&appid=3a0cc64f74febe3d2b029f4d03b00c0f
 
 
 // load local weather
 // show local weather conditions, temperature, humidity, wind speed, UV index
 // favorable, moderate, or severe
-$("#current-day") 
-$("current-date")
-$("#current-city")
-$("#icon-today")
-$("#current-temp")
-$("#current-humidity")
-$("#current-wind")
-$("#current-uv")
+var dayOfWeek = document.getElementById("current-day");
+var dateToday = document.getElementById("current-date");
+var city = document.getElementById("current-city");
+var icon = document.getElementById("icon-today");
+var tempNow = document.getElementById("current-temp");
+var humidityNow = document.getElementById("current-humidity");
+var windNow = document.getElementById("current-wind");
+var uvNow = document.getElementById("current-uv");
+
 
 // 5-day forecast that displays the date, an icon representation of weather conditions, temperature, wind speed,  humidity
-$("day")
-$("date")
-$("icon")
-$("temp")
-$("humidity")
-$("wind")
+var futureDay = document.querySelector(".day");
+var futureDate = document.querySelector(".date");
+var futureIcon = document.querySelector(".icon");
+var futureTemp = document.querySelector(".temp");
+var futureHumidity = document.querySelector(".humidity");
+var futureWind = document.querySelector(".wind");
 
 
 // search a different city
@@ -30,64 +31,65 @@ $("wind")
 
 // save city search history to local storage
 $(document).ready(function () {
-	$("#city").val(localStorage.getItem("city"));
+  $("#city").val(localStorage.getItem("city"));
 });
-// search any city
-$("#search-btn").click(function(){
+// click button to get weather
+$("#search-btn").click(function () {
 
-// link to weather info
+  // link to weather info
 
-  	localStorage.setItem("city", document.getElementById("city").value);
+  localStorage.setItem("city", document.getElementById("city").value);
 });
-
-
-
-// $(document).ready(function () {
-// 	$("#text11").val(localStorage.getItem("text11"));
-// });
-// saveSchedule11El.addEventListener('click', function () {
-// 	localStorage.setItem("text11", document.getElementById("text11").value);
-// });
-
-
 
 
 // click pre-selected city
 // save city to local storage
 
 // search Atlanta
-$("#atlanta").click(function(){
+$("#atlanta").click(function () {
+
   console.log("atl")
 });
 
 // search Boston
-$("#boston").click(function(){
-  console.log("click")
+$("#boston").click(function () {
+  console.log("boston")
 });
 
 // search Chicago
-$("#chicago").click(function(){
-  console.log("click")
+
+// var apiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + 33.44 + "&lon=" + -94.04 + "&exclude=minutely,hourly&appid=3a0cc64f74febe3d2b029f4d03b00c0f";
+
+var apiUrlChi ="https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&exclude=minutely,hourly&appid=3a0cc64f74febe3d2b029f4d03b00c0f"
+var getWeather = function() {
+  fetch(apiUrlChi).then(function(response) {
+    response.json().then(function(data) {
+      console.log(data);
+    });
+  });
+};
+
+$("#chicago").click(function () {
+  getWeather();
+  console.log("chi")
 });
 
+
+
 // search Dallas
-$(".dallas").click(function(){
-  console.log("click")
+$(".dallas").click(function () {
+  console.log("dal")
 });
 
 // Search Houston
-$("#houston").click(function(){
-  console.log("click")
+$("#houston").click(function () {
+  console.log("hou")
 });
 
 // search Washington, DC
-$("#washington-dc").click(function(){
-  console.log("click")
+$("#washington-dc").click(function () {
+  console.log("DC")
 });
-
-
-
-
 
 
 // click on a city in the search history
