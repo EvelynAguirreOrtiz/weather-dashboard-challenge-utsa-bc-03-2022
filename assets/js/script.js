fc3a1fba4b7e7429271c07af09fbf799369b2a8e
Lash Blast <evelyn.aguirre.9895@gmail.com>
2022-03-14 11:51:25 -0500

    confirm button operation

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,8 +5,9 @@
 // load local weather
 // show local weather conditions, temperature, humidity, wind speed, UV index
 // favorable, moderate, or severe
-$("#day-date")
-$("#city")
+$("#current-day") 
+$("current-date")
+$("#current-city")
 $("#icon-today")
 $("#current-temp")
 $("#current-humidity")
@@ -27,8 +28,56 @@ $("wind")
 // search saved to local storage
 // 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
 
+// save search history to local storage
+$(document).ready(function () {
+	$(".search-text").val(localStorage.getItem("search-text"));
+});
+// search for city
+$("#search-btn").click(function(){
+  	localStorage.setItem(".search-text", $(".search-text").value);
+
+    console.log("click")
+});
+
+
+
 // click pre-selected city
 // save city to local storage
+
+// click Atlanta
+$("#atlanta").click(function(){
+  console.log("atl")
+});
+
+// click Boston
+$("#boston").click(function(){
+  console.log("click")
+});
+
+// click Chicago
+$("#chicago").click(function(){
+  console.log("click")
+});
+
+// Click Dallas
+$(".dallas").click(function(){
+  console.log("click")
+});
+
+// Search Houston
+$("#houston").click(function(){
+  console.log("click")
+});
+
+// search Washington, DC
+$("#washington-dc").click(function(){
+  console.log("click")
+});
+
+
+
+
+
 
 // click on a city in the search history
 // THEN I am again presented with current and future conditions for that city
