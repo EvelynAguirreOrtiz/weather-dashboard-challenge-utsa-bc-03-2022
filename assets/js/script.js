da315991f72e5468e8c926c6f2d383f17d19f609
Lash Blast <evelyn.aguirre.9895@gmail.com>
2022-03-13 20:24:18 -0500

    refine html

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+// weather conditions, the temperature, the humidity, the wind speed, and the UV index
+// favorable, moderate, or severe
+// future weather conditions
+// 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
+
+
+// other city
+// 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
+
+// click on a city in the search history
+// THEN I am again presented with current and future conditions for that city
