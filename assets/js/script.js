a579fc69bf690763fefae9c000652aaa4230041a
Lash Blast <evelyn.aguirre.9895@gmail.com>
2022-03-14 00:24:13 -0500

    refine layout and set selectors

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,34 @@
-// weather conditions, the temperature, the humidity, the wind speed, and the UV index
+// 7 day forecast w alerts
+// https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
+
+
+// load local weather
+// show local weather conditions, temperature, humidity, wind speed, UV index
 // favorable, moderate, or severe
-// future weather conditions
-// 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
+$("#day-date")
+$("#city")
+$("#icon-today")
+$("#current-temp")
+$("#current-humidity")
+$("#current-wind")
+$("#current-uv")
+
+// 5-day forecast that displays the date, an icon representation of weather conditions, temperature, wind speed,  humidity
+$("day")
+$("date")
+$("icon")
+$("temp")
+$("humidity")
+$("wind")
 
 
-// other city
+// search a different city
+// click "search" to choose city
+// search saved to local storage
 // 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
+
+// click pre-selected city
+// save city to local storage
 
 // click on a city in the search history
 // THEN I am again presented with current and future conditions for that city
